0f67ee7aaf6cccd74190ce4b60f839b76a44a6fa
yanxiaodi <929213769@qq.com>
2017-02-27 16:04:28 +0800

    :tada:  first commit

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "off",
+            "tab"
+        ],
+        "linebreak-style": [
+            "warn",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ]
+    }
+};
---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+#app
+    img( src="./assets/logo.png" )
+    router-view
+</template>
+
+<script>
+export default {
+    name: 'app'
+}
+</script>
+
+<style lang="sass" scoped>
+#app
+    text-align: center
+    color: #2c3e50
+    margin-top: 60px
+</style>
