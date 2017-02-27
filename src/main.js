e3d911c7d2db4812c3a5678529672e3e6d3ba4a6
yanxiaodi <929213769@qq.com>
2017-02-27 16:54:12 +0800

    :heavy_plus_sign: add Element-UI

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue          from 'vue'
+import App          from './App'
+import router       from './router'
+
+import ElementUI    from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
 
 Vue.config.productionTip = false
+Vue.use( ElementUI )
 
 /* eslint-disable no-new */
 new Vue({
