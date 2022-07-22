b469265b8ec8a08346c18154bd57305684ce8905
yanxiaodi <929213769@qq.com>
2022-07-22 11:31:33 +0800

    Config Pug & Stylus

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,14 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+<template lang="pug">
+.about
+  h1 This is an about page
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
+<style lang="stylus">
+@media (min-width 1024px)
+  .about
+    min-height 100vh
+    display flex
+    align-items center
+    h1
+      color red
 </style>
